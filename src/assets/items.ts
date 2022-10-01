d427999c494a4cdfff0444f3002433695bb25563
Charlie Algert <38013771+algertc@users.noreply.github.com>
2022-10-01 00:27:20 -0700

    Update items.ts

--- src/assets/items.ts
+++ src/assets/items.ts
@@ -9,22 +9,16 @@ import shockpoint from '../assets/shockpoint (4).png'
 import { ItemType } from '../ts';
 
 const items: ItemType[] = [{
-    "title": "Website",
-    "subtitle": "Look at my work!",
-    "image": website,
-    "link": "https://charliealgert.com" //your personal website or portfolio  link
+    "title": "LinkedIn",
+    "subtitle": "Professional Background",
+    "image": linkedin,
+    "link": "https://www.linkedin.com/in/charliealgert/" // linkedin
 },
 {
     "title": "GitHub",
-    "subtitle": "@algertc| 🏡 to all my open-source projects",
+    "subtitle": "All my open-source projects",
     "image": github,
     "link": "https://github.com/algertc" //Github Profile link
-},
-{
-    "title": "Instagram",
-    "subtitle": "@charlie.algert",
-    "image": instagram,
-    "link": "https://instagram.com/algertc" //instagram profile link 
 },
 {
     "title": "Research",
@@ -33,29 +27,22 @@ const items: ItemType[] = [{
     "link": "https://www.polygence.org/scholars/charlie-algert"// polygence profile link 
 },
 {
-    "title": "LinkedIn",
-    "subtitle": "Professional Background",
-    "image": linkedin,
-    "link": "https://www.linkedin.com/in/charliealgert/" // linkedin
-},
-{
     "title": "Tor Atlas",
     "subtitle": "View My Relay Stats",
     "image": tor,
     "link": "https://metrics.torproject.org/rs.html#details/5D410EB085D4A8FBA22305FBA8DC19B71D9387FC"//atlas  link 
 },
-
 {
     "title": "ShockPoint",
-    "subtitle": "Check Out The Website ",
+    "subtitle": "Check Out The Website",
     "image": shockpoint,
     "link": "https://shockpointmedia.com" 
 },
 {
-    "title": "Telegram",
-    "subtitle": "",
-    "image": telegram,
-    "link": "https://telegram.me/" //Change To Car Progress Blog 
+    "title": "Instagram",
+    "subtitle": "@charlie.algert",
+    "image": instagram,
+    "link": "https://instagram.com/algertc" //instagram profile link 
 }]
 
 export default items
