44957e5681d542596acb48f8e40aae23dd6239a2
Charlie Algert <38013771+algertc@users.noreply.github.com>
2022-10-01 00:11:04 -0700

    Update items.ts

--- src/assets/items.ts
+++ src/assets/items.ts
@@ -1,11 +1,11 @@
 import website from '../assets/website.png'
 import instagram from '../assets/instagram.png'
 import linkedin from '../assets/linkedin.jpg'
-import youtube from '../assets/youtube.jpeg'
-import dribbble from '../assets/dribbble.png'
+import polygence from '../assets/polygence.png'
+import tor from '../assets/tor_navy.png'
 import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
-import twitter from '../assets/twitter.png'
+import shockpoint from '../assets/shockpoint (4).png'
 import { ItemType } from '../ts';
 
 const items: ItemType[] = [{
@@ -27,29 +27,29 @@ const items: ItemType[] = [{
     "link": "https://instagram.com/algertc" //instagram profile link 
 },
 {
-    "title": "Twitter",
-    "subtitle": "@heysagnik | 😉",
-    "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
+    "title": "Research",
+    "subtitle": "Polyvence Scholar Profile",
+    "image": polygence,
+    "link": "https://www.polygence.org/scholars/charlie-algert"// polygence profile link 
 },
 {
     "title": "LinkedIn",
-    "subtitle": "Professional Endeavors 👨‍💼",
+    "subtitle": "Professional Background",
     "image": linkedin,
     "link": "https://www.linkedin.com/in/charliealgert/" // linkedin
 },
 {
-    "title": "Research",
-    "subtitle": "Polygence | Scholar Profile",
-    "image": youtube,
+    "title": "Tor Atlas",
+    "subtitle": "View My Relay Stats",
+    "image": tor,
     "link": "https://www.polygence.org/scholars/charlie-algert"//polygence  link 
 },
 
 {
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | shots of dezigns ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+    "title": "ShockPoint",
+    "subtitle": "Check Out The Website ",
+    "image": shockpoint,
+    "link": "https://shockpointmedia.com" 
 },
 {
     "title": "Telegram",
